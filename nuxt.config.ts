fc4e715974c59cc05f4cde1cb67b59c7d2d00275
yuu-eguci <yuu.eguci@gmail.com>
2023-03-11 14:05:23 +0900

    🎨 【inject のおさらい】 開発環境でのみ console.log する plugin を inject してみる

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,7 @@ const nuxtConfig: NuxtConfig = {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '@/plugins/debugLog.ts', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
