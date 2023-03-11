375e2566ba439e95367b5ad47b1ecc02f7dddb77
yuu-eguci <yuu.eguci@gmail.com>
2023-03-11 14:19:05 +0900

    🎨 【page + container + presentational のおさらい】 基本的な page + component を定義しておく

---
+++ components/sample-page/presentational/TheSampleView.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em> {{ randomData }}
+          </small>
+          <v-btn @click="foo">
+            Foo って console するよ
+          </v-btn>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'TheSampleView',
+  props: {
+    getRandomData: {
+      //                                    ↓返り値
+      type: Function as Vue.PropType<() => string>,
+      required: true
+    },
+    alan: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    randomData (): string {
+      return this.getRandomData()
+    },
+    a (): string {
+      return this.alan
+    }
+  },
+  methods: {
+    foo (): void {
+      // Property 'getRandomData' does not exist on type 'Vue'.Vetur(2339)
+      // computed.randomData では this.getRandomData で使えてるのに。
+      // ↓
+      // computed と methods で同じ props (Function でも String でも) 使ってて、
+      // computed のほうで返り値の型 (: string とか) を書いてないと、 methods のほうで
+      // 上述エラーが出る………
+      // eslint-disable-next-line
+      console.info(this.getRandomData())
+      // console.info(this.$props.getRandomData())
+    },
+    getAlan () {
+      // eslint-disable-next-line
+      console.info(this.alan)
+    }
+  }
+})
+</script>
---
+++ pages/sample-page/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <TheSampleContainer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { foo, bar } from '@/utils/FooUtils'
+import { Baz } from '@/utils/BazUtils'
+import TheSampleContainer from '@/components/sample-page/container/TheSampleContainer.vue'
+
+// NOTE: vue-cli と近い使用感を実現するため Object Style で実装します。
+export default Vue.extend({
+  name: 'SamplePage',
+  components: {
+    TheSampleContainer
+  },
+  mounted () {
+    // 動作確認用、意味のないコード
+    // const foo: string = ''
+    // console.log(foo)
+
+    // eslint-disable-next-line
+    console.info(foo())
+    // eslint-disable-next-line
+    console.info(bar())
+    // eslint-disable-next-line
+    console.info(new Baz(1, 2))
+  }
+})
+</script>
