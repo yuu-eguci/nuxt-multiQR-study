8c8c63d82e2e7a4f9d696883fc3a2bea03eda48c
yuu-eguci <yuu.eguci@gmail.com>
2023-03-11 21:32:26 +0900

    ✨ とりあえず QRReaderPage

---
+++ components/qr-reader/container/QRReaderContainer.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Vue, { CreateElement, VNode } from 'vue'
+import QRReaderView from '../presentational/QRReaderView.vue'
+
+// NOTE: API アクセスのコードは container に書くよ。
+export default Vue.extend({
+  methods: {
+    // API access
+    getRandomData (): string {
+      return 'foo'
+    }
+  },
+  // render (h) { だと、 Property 'getRandomData' does not exist on type 'CombinedVueInstance が出る
+  render (h: CreateElement): VNode {
+    return h(QRReaderView, {
+      props: {
+        getRandomData: this.getRandomData,
+        alan: 'alan'
+      }
+    })
+  }
+})
+</script>
---
+++ pages/qr-reader/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <QRReaderContainer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import QRReaderContainer from '@/components/qr-reader/container/QRReaderContainer.vue'
+
+// NOTE: vue-cli と近い使用感を実現するため Object Style で実装します。
+export default Vue.extend({
+  name: 'QRReaderPage',
+  components: {
+    QRReaderContainer
+  },
+  mounted () {
+  }
+})
+</script>
