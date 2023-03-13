7e6d2772903c10379dbce2bbcb493af9905964e7
yuu-eguci <yuu-eguci@users.noreply.github.com>
2023-03-13 00:36:44 +0000

    deploy: b73226f65d90d21894c184a28b3accc5d14d36e3

---
+++ _nuxt/d06797e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{454:function(e){e.exports=JSON.parse('{"indexPage":{"theCanvas":{"theHandwriting":"Handwriting","draw":"Draw","eraser":"Eraser","reset":"Reset","download":"Download","setImageA":"Set image A","setImageB":"Set image B"}}}')}}]);
